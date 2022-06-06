b814ba2d9134d533227757c61229855fcb70eae0
junsu_Kim <96895747+junsu22@users.noreply.github.com>
2022-06-06 14:14:51 +0900

    add

--- index.js
+++ index.js
@@ -1,42 +1,92 @@
 const { ApolloServer, gql } = require('apollo-server');
-const {readFileSync} = require('fs');
-const { join } = require('path');
+const {readFileSync} = require("fs");
+const { join } = require("path");
+
 // The GraphQL schema
-// typeDefs :schema 정의
-const typeDefs = gql`       
-  type Query {
-    "A simple type for getting started!"
-    hello: String,
-    books : [Book]    
-    book(bookId : Int): Book   
-  }
-  type Mutation {
-    addBook
-  }
-  
-  type Book{
-    bookId:Int
-    title:String
-    message:String
+const typeDefs = gql`
+type Query {
+  "A simple type for getting started!"
+  hello: String
+  books: [Book]
+  book(bookId: Int): Book
+}
+type Mutation {
+  addBook(title: String, message: String, author: String, url: String): Book
+  editBook(
+    bookId: Int
+    title: String
+    message: String
     author: String
-    url: String 
-  }`;
+    url: String
+  ): Book
+  deleteBook(bookId: Int): Book
+}
+type Book {
+  bookId: Int
+  title: String
+  message: String
+  author: String
+  url: String
+}
+`;
 
-
-
-
-// schema 에 해당하는 구현 조회,수정,삭제 
+// A map of functions which return data for the schema.
 const resolvers = {
   Query: {
-    hello: () => 'world',
-    books:() => {
-      return JSON.parse(readFileSync(join(__dirname, 'books.json')).toString())
+    hello: () => "world",
+    books: () => {
+      return JSON.parse(readFileSync(join(__dirname, "books.json")).toString());
     },
     book: (parent, args, context, info) => {
-      const books = JSON.parse(readFileSync(join(__dirname, 'books.json')).toString())
-      return books.find(book => book.bookId === args.bookId);
-        
-    }
+      const books = JSON.parse(
+        readFileSync(join(__dirname, "books.json")).toString()
+      );
+      return books.find((book) => book.bookId === args.bookId);
+    },
+  },
+  Mutation: {
+    addBook: (parent, args, context, info) => {
+      const books = JSON.parse(
+        readFileSync(join(__dirname, "books.json")).toString()
+      );
+      const maxId = Math.max(...books.map((book) => book.bookId));
+      const newBook = { ...args, bookId: maxId + 1 };
+      writeFileSync(
+        join(__dirname, "books.json"),
+        JSON.stringify([...books, newBook])
+      );
+      return newBook;
+    },
+    editBook: (parent, args, context, info) => {
+      const books = JSON.parse(
+        readFileSync(join(__dirname, "books.json")).toString()
+      );
+
+      const newBooks = books.map((book) => {
+        if (book.bookId === args.bookId) {
+          return args;
+        } else {
+          return book;
+        }
+      });
+
+      writeFileSync(join(__dirname, "books.json"), JSON.stringify(newBooks));
+
+      return args;
+    },
+    deleteBook: (parent, args, context, info) => {
+      const books = JSON.parse(
+        readFileSync(join(__dirname, "books.json")).toString()
+      );
+
+      const deleted = books.find((book) => book.bookId === args.bookId);
+
+      const newBooks = books.filter((book) => book.bookId !== args.bookId);
+
+      writeFileSync(join(__dirname, "books.json"), JSON.stringify(newBooks));
+
+      return deleted;
+    },
   },
 };
 
